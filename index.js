53adfd3c4b40b6a5be825378697534f77cbe5ede
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-11 10:40:18 -0500

    commit tree 21d98aee0f54c6a2f0019434d39fdf4d37ce6b19

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   tutorials.map(tutorial => {
+//     tutorial = tutorial.split(" ");
+//     tutorial.map(word => word.charAt(0).toUppercase() + word.slice(1));
+//     tutorial.join(" ");
+//   });
+// }
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(string => string.split(" ").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" "))
 }
